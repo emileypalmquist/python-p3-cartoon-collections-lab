a7ee4f5fd16f6babc65b9fad87ce3642d624281e
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-03 10:52:33 -0700

    tests passing

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,34 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves_names):
+    for index, name in enumerate(dwarves_names):
+        print(f"{index + 1}. {name}")
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def capitalize_call(call):
+    # return call[0].upper() + call[1:] + "!"
+    return call.title() + "!"
 
-def find_the_cheese():
-    pass
+
+def summon_captain_planet(planeteer_calls):
+    return [capitalize_call(call) for call in planeteer_calls]
+
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+
+    return False
+
+
+def find_the_cheese(foods):
+    cheeses = [
+        "cheddar",
+        "gouda",
+        "camembert"
+    ]
+
+    for cheese in cheeses:
+        if cheese in foods:
+            return cheese
+
+    return None
